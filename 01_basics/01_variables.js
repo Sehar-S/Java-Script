eab6beb9906cfbefd592c4df36a7b320dddfe054
Sehar Shahzadi <mishaleshy8@gmail.com>
2026-06-25 15:35:29 +0000

    done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 1234567
+let accountEmail = "sehar@google.com"
+var accountPassword = "3456884"
+accountCity = pakistan
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+
+
+
+console.log(accountId);
