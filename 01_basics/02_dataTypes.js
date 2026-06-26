c4bca86fe882934673059482716666b46e696cbc
Sehar Shahzadi <mishaleshy8@gmail.com>
2026-06-26 06:16:49 +0000

    dataType

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+let name = "sehar"
+let age = 20
+let isLoggedIn = true
+let city;
+
+
+// Check dataType Use typeof
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+console.log(typeof city);
+console.log(typeof null);
+console.log(typeof undefined);
+
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
