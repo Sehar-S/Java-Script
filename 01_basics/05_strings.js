74d3dd974b8dde5a117c2eea1a57080b24b34233
Sehar Shahzadi <mishaleshy8@gmail.com>
2026-07-05 07:23:58 +0000

    Done

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,5 @@
+const name = "Sehar"
+const repoCount =  50
+//console.log(name +  repoCount + " value");  outdated way to concatenate strings - avoid like this
+// new way of doing it ``backpicks inside ${} we can use variables and expressions is called string-interpolation 
+console.log(`Hello ${name} you have ${repoCount} repos`)
