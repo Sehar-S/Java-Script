eb09d4afd35b91fd73bb8f63d44797b9fa642cbe
Sehar Shahzadi <mishaleshy8@gmail.com>
2026-07-05 06:32:54 +0000

    Done

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -1,16 +1,16 @@
-// console.log(2 > 1);
-// console.log(2 >= 1);
-// console.log(2 < 1);
-// console.log(2 == 1);
-// console.log(2 != 1);
+//  console.log(2 > 1);
+//  console.log(2 >= 1);
+//  console.log(2 < 1);
+//  console.log(2 == 1);
+//  console.log(2 != 1);
 
 
-// console.log("2" > 1);
-// console.log("02" > 1);
+//  console.log("2" > 1);
+//  console.log("02" > 1);
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
+//  console.log(null > 0);
+//  console.log(null == 0);
+//  console.log(null >= 0);
 
 console.log(undefined == 0);
 console.log(undefined > 0);
@@ -19,3 +19,4 @@ console.log(undefined < 0);
 // === 
 
 console.log("2" === 2);
+
